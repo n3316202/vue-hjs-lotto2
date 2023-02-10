b490a6e6ff672fded5f57a7777ea3c6b3de44347
DESKTOP-VQVDMU6\USER <nqwrt@naver.com>
2023-02-10 13:08:00 +0900

    vue 게시판 구현

--- src/components/rsp/RspMain.vue
+++ src/components/rsp/RspMain.vue
@@ -10,12 +10,7 @@
               <!-- <button class="btn btn-primary m-2 your-rsp">가위</button>
                 <button class="btn btn-primary m-2 your-rsp">바위</button>
                 <button class="btn btn-primary m-2 your-rsp">보</button> -->
-              <button
-                v-for="(rsp, index) in rspArr"
-                :key="index"
-                class="btn btn-primary m-2"
-                @click="setUserRsp"
-              >
+              <button v-for="(rsp, index) in rspArr" :key="index" class="btn btn-primary m-2" @click="setUserRsp">
                 {{ rsp }}
               </button>
             </div>
@@ -23,11 +18,7 @@
         </div>
         <div class="col-md-4 d-flex justify-content-center">
           <div class="card" style="width: 18rem">
-            <img
-              src="https://taegon.kim/wp-content/uploads/2018/05/image-5.png"
-              class="card-img-top"
-              alt="..."
-            />
+            <img src="https://taegon.kim/wp-content/uploads/2018/05/image-5.png" class="card-img-top" alt="..." />
             <div class="card-body text-center">
               <h5 class="card-title">심판</h5>
               <p>{{ result }}</p>
@@ -39,9 +30,7 @@
             <img :src="computer.img" class="card-img-top" alt="..." />
             <div class="card-body text-center">
               <h5 class="card-title">컴퓨터</h5>
-              <button id="rsp-btn-computer" class="btn btn-primary m-2">
-                가위
-              </button>
+              <button id="rsp-btn-computer" class="btn btn-primary m-2">가위</button>
             </div>
           </div>
         </div>
@@ -51,75 +40,71 @@
 </template>
 
 <script>
-import { ref, reactive } from "vue";
+import { ref, reactive } from 'vue'
 
 export default {
   setup() {
-    const imgArr = [
-      require("@/assets/img/scissor.jpg"),
-      require("@/assets/img/rock.jpg"),
-      require("@/assets/img/paper.jpg"),
-    ];
-    const rspArr = ref(["가위", "바위", "보"]);
-    let result = ref("");
+    const imgArr = [require('@/assets/img/scissor.jpg'), require('@/assets/img/rock.jpg'), require('@/assets/img/paper.jpg')]
+    const rspArr = ref(['가위', '바위', '보'])
+    let result = ref('')
 
     const user = reactive({
-      name: "user",
+      name: 'user',
       rsp: 0,
-      img: "https://taegon.kim/wp-content/uploads/2018/05/image-5.png",
-    });
+      img: 'https://taegon.kim/wp-content/uploads/2018/05/image-5.png'
+    })
     const computer = reactive({
-      name: "computer",
+      name: 'computer',
       rsp: 0,
-      img: "https://taegon.kim/wp-content/uploads/2018/05/image-5.png",
-    });
+      img: 'https://taegon.kim/wp-content/uploads/2018/05/image-5.png'
+    })
 
     const setUserRsp = (event) => {
-      console.log(event.target.innerText);
-      user.rsp = rspArr.value.indexOf(event.target.innerText);
-      user.img = imgArr[user.rsp];
+      console.log(event.target.innerText)
+      user.rsp = rspArr.value.indexOf(event.target.innerText)
+      user.img = imgArr[user.rsp]
 
-      console.log(user.rsp);
-      console.log(user.img);
+      console.log(user.rsp)
+      console.log(user.img)
 
-      result.value = resultRsp();
-      console.log(result.value);
-    };
+      result.value = resultRsp()
+      console.log(result.value)
+    }
 
     const resultRsp = () => {
-      let result = "";
-      computer.rsp = randomRsp();
-      computer.img = imgArr[computer.rsp];
+      let result = ''
+      computer.rsp = randomRsp()
+      computer.img = imgArr[computer.rsp]
 
-      console.log(computer.rsp);
+      console.log(computer.rsp)
       if (user.rsp == computer.rsp) {
-        console.log("비겻습니다.");
-        result = "비겻습니다.";
-        return result;
+        console.log('비겻습니다.')
+        result = '비겻습니다.'
+        return result
       }
 
-      if (rspArr.value[computer.rsp] == "가위") {
-        if (rspArr.value[user.rsp] == "바위") result = "당신이 이겻니다.";
-        else if (rspArr.value[user.rsp] == "보") result = "당신이 졋습니다";
-      } else if (rspArr.value[computer.rsp] == "바위") {
-        if (rspArr.value[user.rsp] == "보") result = "당신이 이겼니다.";
-        else if (rspArr.value[user.rsp] == "가위") result = "당신이 졌습니다";
-      } else if (rspArr.value[computer.rsp] == "보") {
-        if (rspArr.value[user.rsp] == "가위") result = "당신이 이겻니다.";
-        else if (rspArr.value[user.rsp] == "바위") result = "당신이 졌습니다";
+      if (rspArr.value[computer.rsp] == '가위') {
+        if (rspArr.value[user.rsp] == '바위') result = '당신이 이겻니다.'
+        else if (rspArr.value[user.rsp] == '보') result = '당신이 졋습니다'
+      } else if (rspArr.value[computer.rsp] == '바위') {
+        if (rspArr.value[user.rsp] == '보') result = '당신이 이겼니다.'
+        else if (rspArr.value[user.rsp] == '가위') result = '당신이 졌습니다'
+      } else if (rspArr.value[computer.rsp] == '보') {
+        if (rspArr.value[user.rsp] == '가위') result = '당신이 이겻니다.'
+        else if (rspArr.value[user.rsp] == '바위') result = '당신이 졌습니다'
       }
 
-      console.log(result);
-      return result;
-    };
+      console.log(result)
+      return result
+    }
 
     const randomRsp = () => {
-      return Math.floor(Math.random() * 3);
-    };
+      return Math.floor(Math.random() * 3)
+    }
 
-    return { rspArr, setUserRsp, user, computer, imgArr, result };
-  },
-};
+    return { rspArr, setUserRsp, user, computer, imgArr, result }
+  }
+}
 </script>
 
 <style></style>
---
+++ src/views/BoardMainView.vue
@@ -0,0 +1,30 @@
+<template>
+  <body>
+    <BoardHeader></BoardHeader>
+    <BoardMain></BoardMain>
+    <BoardFooter></BoardFooter>
+  </body>
+</template>
+
+<script>
+import BoardHeader from '../components/LottoHeader.vue'
+import BoardMain from '@/components/board/BoardMain.vue'
+import BoardFooter from '../components/LottoFooter.vue'
+
+export default {
+  components: {
+    BoardHeader,
+    BoardMain,
+    BoardFooter
+  }
+}
+</script>
+
+<style scoped>
+body {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+</style>
